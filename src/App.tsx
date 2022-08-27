46f683850ed695e063a9311b53381d40745d4ce3
Eduardo Andrade <edu2andrade@gmail.com>
2022-08-27 08:00:19 +0100

    eslint configured

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,10 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/themes/global'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
       <GlobalStyle />
     </ThemeProvider>
   )
-};
+}
